84c860cc60f60729e5eebb72d2227847bdb4e10b
Christopher Rivera <christopher.rivera27@gmail.com>
2018-10-16 21:02:18 -0700

    properly linked out PNG files

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
     var myArray = [
-        '../images/joyeaux_card_1.png', 
-        '../images/joyeaux_card_2.png',
-        '../images/joyeaux_card_3.png', 
-        '../images/joyeaux_card_4.png', 
-        '../images/joyeaux_card_5.png', 
-        '../images/joyeaux_card_6.png',
-        '../images/joyeaux_card_7.png'
+        'https://christopher-rivera.github.io/images/joyeaux_card_1.png', 
+        'https://christopher-rivera.github.io/images/joyeaux_card_2.png',
+        'https://christopher-rivera.github.io/images/joyeaux_card_3.png', 
+        'https://christopher-rivera.github.io/images/joyeaux_card_4.png', 
+        'https://christopher-rivera.github.io/images/joyeaux_card_5.png', 
+        'https://christopher-rivera.github.io/images/joyeaux_card_6.png',
+        'https://christopher-rivera.github.io/images/joyeaux_card_7.png'
     ]    
     
     function changeImage() {
